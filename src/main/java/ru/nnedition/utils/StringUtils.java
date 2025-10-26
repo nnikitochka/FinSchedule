db27589351db5356ece5e21c3797d576c743220f
nnikitochka <asopiasoska@gmail.com>
2025-10-26 19:30:07 +0700

    Новый утилитарный метод

--- src/main/java/ru/nnedition/utils/StringUtils.java
+++ src/main/java/ru/nnedition/utils/StringUtils.java
@@ -4,7 +4,13 @@ import org.jetbrains.annotations.NotNull;
 
 public class StringUtils {
     @NotNull
-    public static String trimEnd(@NotNull String str) {
+    public static String trimEnd(@NotNull final String str) {
         return str.replaceFirst("\\s+$", "");
+    }
+
+    @NotNull
+    public static String removeSuffix(@NotNull final String str, @NotNull final String suffix) {
+        if (!str.endsWith(suffix)) return str;
+        return str.substring(0, str.length() - suffix.length());
     }
 }
