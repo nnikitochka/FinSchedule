4320b07b65c1060e75646f0f44f2c377c192a559
nnikitochka <asopiasoska@gmail.com>
2025-10-26 19:28:18 +0700

    Исправлен баг с неизменяемостью списка подгрупп

--- src/main/java/ru/nnedition/finschedule/schedule/groups/Group.java
+++ src/main/java/ru/nnedition/finschedule/schedule/groups/Group.java
@@ -2,14 +2,15 @@ package ru.nnedition.finschedule.schedule.groups;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record Group(
         @NotNull String name,
-        @NotNull List<Integer> subGroups
+        @NotNull ArrayList<Integer> subGroups
 ) {
     public Group(@NotNull String name) {
-        this(name, List.of());
+        this(name, new ArrayList<>());
     }
 
     @NotNull
